da1d24057f0044da56c1994d6d69c6b2394c81cc
Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
2021-04-20 18:52:25 -0300

    creating component player end server.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
-import { Header } from "../components";
+
+
+import { useEffect} from 'react';
 
 export default function Home() {
+
+  useEffect(()=>{
+    fetch('http://localhost:3333/episodes')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  },[]);
   return (
-    <Header/>
+    <>
+      <h1>index</h1>
+     
+    </>
   )
 }
