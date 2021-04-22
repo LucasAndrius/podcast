d8cade6a38f9ce0631baae511ccf3c9bab79ef08
Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
2021-04-22 11:54:24 -0300

    add audio

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,7 @@ function MyApp({ Component, pageProps }) {
 
   const [episodeList,setEpisodeList] = useState([]);
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(true);
 
 
   function play(episode) {
@@ -20,7 +21,7 @@ function MyApp({ Component, pageProps }) {
   }
 
   return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying}}>
       <div className={styles.wrapper}>
         <main>
           <Header/>
